1c7ac74bb69e0cb0bc4ed8670b981409f9800d77
Z_Shobbar <zoha.shobbar@outlook.com>
2022-12-02 22:54:32 +0330

    add integration tests

---
+++ tests/Intsoft.Exam.IntegrationTests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Intsoft.Exam.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Intsoft.Exam.IntegrationTests
+{
+    public class BaseTest
+    {
+        protected readonly HttpClient TestClient;
+        protected BaseTest()
+        {
+            var appFactory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
